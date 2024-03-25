79c3735d49b05e7d175146bc1aa713ff6ea6e878
AlexDeLos <alexdelos2001@gmail.com>
2024-03-25 15:37:51 +0100

    No more wrning suppression

--- Utils/api.py
+++ Utils/api.py
@@ -124,9 +124,7 @@ def clean_logs(contract, myevent, log):
     eval_string = 'contract.events.{}().processReceipt({})'.format(myevent, log_dict)
     try:
         # suppress user warnings here
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            args_event = eval(eval_string)
+        args_event = eval(eval_string)
         args_event = args_event[0]
         t = ''
     except IndexError as e:
