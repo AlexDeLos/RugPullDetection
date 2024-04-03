3cb5b0ab6d00559991acaea50d48abdf6a06067d
AlexDeLos <alexdelos2001@gmail.com>
2024-04-03 13:23:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ print('created source_code')
 if not os.path.exists(out_path + "/Token_tx"):
     os.makedirs(out_path + "/Token_tx")
 
-step_size = 500
+step_size = 5000
 count = 0
 #* run get_transfers.py
 
