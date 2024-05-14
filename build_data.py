710a8a104cc7a5b88cba65c3d487b1c213ceca6b
AlexDeLos <alexdelos2001@gmail.com>
2024-05-14 14:58:55 +0200

    Update build_data.py

--- build_data.py
+++ build_data.py
@@ -223,7 +223,7 @@ try:
     if not os.path.exists(out_path + "/Token_tx"):
         os.makedirs(out_path + "/Token_tx")
 
-    step_size = 5000
+    step_size = 1000
     count = 0
     #* run get_transfers.py
 
